c02156e884abad82f9d4ea477a491a137a3763ba
Roben Kleene <contact@robenkleene.com>
2019-03-12 18:14:14 -0400

    Work on bash script

--- repla.sh
+++ repla.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-plugin_name="$1"
-dir=$PWD
+bundle_name="$1"
 
+if [[ -z "$bundle_name" ]]; then
+  echo "Error no bundle name specified" >&2
+  exit 1
+fi
+
+dir=$PWD
 env=$(env)
 env_arg=$(printf %q $env)

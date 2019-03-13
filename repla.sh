a5f38286b7663f851c312b98ccc873acac8bbe93
Roben Kleene <contact@robenkleene.com>
2019-03-13 12:26:35 -0400

    Finish first draft of script

--- repla.sh
+++ repla.sh
@@ -5,10 +5,17 @@ set -e
 bundle_name="$1"
 
 if [[ -z "$bundle_name" ]]; then
-  echo "Error no bundle name specified" >&2
+  echo "Error no bundle name specified." >&2
+  exit 1
+fi
+
+if [[ "$bundle_name" = "Server" ]]; then
+  echo "The only the Server bundle is currently supported." >&2
   exit 1
 fi
 
 dir=$PWD
 env=$(env)
 env_arg=$(printf %q $env)
+
+osascript "${BASH_SOURCE%/*}/run_plugin.scpt" $@ $env_arg

4d868abee1f16e65a9eb98d0c5c8e45aeeb92aae
Roben Kleene <contact@robenkleene.com>
2019-10-30 15:41:24 -0400

    Use bundled Ruby (#9)
    * Move to shell wrapper
    
    * Suppress output and fix linter errors
    
    * Add binaries
    
    * Remove wrong submodule
    
    * Add bin
    
    * Move to scripts
    
    * Run setup Ruby
    
    * Fix linter errors
    
    * Add install and uninstall scripts
    
    * Fix getting path
    
    * Use full path
    
    * Use full path
    
    * Improve install
    
    * Improve path

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY: ci ac autocorrect lint
+SYMLINK_PATH = /usr/local/bin/repla
+ORIGINAL_PATH = /Applications/Repla.app/Contents/Resources/Scripts/repla
+LOCAL_PATH = Scripts/repla
+
+.PHONY: ci ac autocorrect lint install_override uninstall_override
 
 ci: lint
 ac: autocorrect
@@ -8,4 +12,12 @@ lint:
 
 autocorrect:
 	rubocop -a
+
+install_override:
+	rm -f "$(SYMLINK_PATH)"
+	ln -s "${PWD}/$(LOCAL_PATH)" "$(SYMLINK_PATH)"
+
+uninstall_override:
+	rm -f "$(SYMLINK_PATH)"
+	ln -s "$(ORIGINAL_PATH)" "$(SYMLINK_PATH)"
 
--- Scripts/repla.rb
+++ Scripts/repla.rb
@@ -1,11 +1,11 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/ruby --disable-gems
+#!/usr/bin/env ruby --disable-gems
 
 require 'shellwords'
 
 usage = 'Usage: repla [plugin name (or) file] [plugin parameters]'
 
 if ARGV[0].nil?
-  STDERR.puts usage
+  warn usage
   exit 1
 end
 
@@ -13,19 +13,21 @@ first_arg = ARGV[0]
 
 if File.exist?(first_arg)
   if ARGV.length > 1
-    STDERR.puts usage
+    warn usage
     exit 1
   end
   script_arg = File.join(__dir__, 'open.scpt')
   path = File.expand_path(first_arg)
-  command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(path)} >/dev/null"
+  command = "/usr/bin/osascript \"#{script_arg}\" "\
+    "\"#{Shellwords.escape(path)}\" '>/dev/null'"
   exec(command)
 end
 
 dir = Dir.pwd
 
 script_arg = File.join(__dir__, 'run_plugin.scpt')
-command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(first_arg)} #{Shellwords.escape(dir)}"
+command = "/usr/bin/osascript \"#{script_arg}\" "\
+  "\"#{Shellwords.escape(first_arg)}\" \"#{Shellwords.escape(dir)}\" >/dev/null"
 
 arguments = ARGV[1..-1]
 command += ' ' + arguments.compact.map(&:to_s).map do |x|

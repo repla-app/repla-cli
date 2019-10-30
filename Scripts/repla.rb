9ddc9e849d386c65d55ec89883a7bb1f3292a0f9
Roben Kleene <contact@robenkleene.com>
2019-10-30 15:39:51 -0400

    Improve path

--- Scripts/repla.rb
+++ Scripts/repla.rb
@@ -18,16 +18,16 @@ if File.exist?(first_arg)
   end
   script_arg = File.join(__dir__, 'open.scpt')
   path = File.expand_path(first_arg)
-  command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(path)} "\
-    '>/dev/null'
+  command = "/usr/bin/osascript \"#{script_arg}\" "\
+    "\"#{Shellwords.escape(path)}\" '>/dev/null'"
   exec(command)
 end
 
 dir = Dir.pwd
 
 script_arg = File.join(__dir__, 'run_plugin.scpt')
-command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(first_arg)} "\
-  "#{Shellwords.escape(dir)} >/dev/null"
+command = "/usr/bin/osascript \"#{script_arg}\" "\
+  "\"#{Shellwords.escape(first_arg)}\" \"#{Shellwords.escape(dir)}\" >/dev/null"
 
 arguments = ARGV[1..-1]
 command += ' ' + arguments.compact.map(&:to_s).map do |x|

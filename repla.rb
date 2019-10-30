99c5fe8262b7fc5b014ba221ee45897c7d2a4fb6
Roben Kleene <contact@robenkleene.com>
2019-10-30 13:22:34 -0400

    Suppress output and fix linter errors

--- repla.rb
+++ repla.rb
@@ -1,4 +1,4 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/ruby --disable-gems
+#!/usr/bin/env ruby --disable-gems
 
 require 'shellwords'
 
@@ -18,14 +18,16 @@ if File.exist?(first_arg)
   end
   script_arg = File.join(__dir__, 'open.scpt')
   path = File.expand_path(first_arg)
-  command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(path)} >/dev/null"
+  command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(path)} "\
+    '>/dev/null'
   exec(command)
 end
 
 dir = Dir.pwd
 
 script_arg = File.join(__dir__, 'run_plugin.scpt')
-command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(first_arg)} #{Shellwords.escape(dir)}"
+command = "/usr/bin/osascript #{script_arg} #{Shellwords.escape(first_arg)} "\
+  "#{Shellwords.escape(dir)} >/dev/null"
 
 arguments = ARGV[1..-1]
 command += ' ' + arguments.compact.map(&:to_s).map do |x|
